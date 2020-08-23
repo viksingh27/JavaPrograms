7df4522767b48ba633f4bf79add0fc39228abeeb
Vikrant Singh <singhvikrant19951227@gmail.com>
2020-08-23 14:32:00 +0530

    Barking dog if else example

---
+++ BarkingDog/src/BarkingDog.java
@@ -0,0 +1,9 @@
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        if(barking && (hourOfDay < 8 && hourOfDay >= 0 || hourOfDay > 22 && hourOfDay <= 23)) {
+            return true;
+        } else  {
+            return false;
+        }
+    }
+}
