9fa50dee1dd8de698e489f0bdc9a1c1494178f24
Vikrant Singh <singhvikrant19951227@gmail.com>
2020-09-29 19:42:09 +0530

    Switch & If Else

---
+++ DayOfTheWeekChallenge/src/DayOfTheWeekChallenge.java
@@ -0,0 +1,57 @@
+public class DayOfTheWeekChallenge {
+    public static void main(String[] args) {
+
+        printDayOfTheWeek(7);
+        printDayOfTheWeek();
+    }
+    private static void printDayOfTheWeek(int day)
+    {
+        switch (day)
+        {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Invalid day");
+        }
+    }
+    private static void printDayOfTheWeek()
+    {
+        int day = 7;
+        if (day == 1)
+        {
+            System.out.println("Monday");
+        }
+        else if (day == 2)
+        {
+            System.out.println("Tuesday");
+        }
+        else if (day == 3)
+        {
+            System.out.println("Wednesday");
+        }
+        else if (day == 4)
+        {
+            System.out.println("Thursday");
+        }
+        else if (day == 5)
+        {
+            System.out.println("Friday");
+        }
+        else if (day == 6)
+        {
+            System.out.println("Saturday");
+        }
+        else if (day == 7)
+        {
+            System.out.println("Sunday");
+        }
+        else
+        {
+            System.out.println("Invalid Day");
+        }
+    }
+}
