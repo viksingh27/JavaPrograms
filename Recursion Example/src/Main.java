f8b4e1ea6e35bcb46c34d3093c119b924de84fb3
viksingh27 <singhvikrant19951227@gmail.com>
2021-11-17 16:51:31 +0530

    Basic oops problem

--- Recursion Example/src/Main.java
+++ Recursion Example/src/Main.java
@@ -2,11 +2,54 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
-            int[] A = {1,2,3,4,5};
-            constructTriangle(A);
+    public static void main(String[] args) {
+
+        int[] nums = {1,0,1,0,1,1,1,0};
+
+        System.out.println(findMaxConsecutiveOnes(nums));
+
     }
+
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+
+                int count = 0;
+                int result = 0;
+            for(int i = 0; i< nums.length; i++)
+            {
+
+                if (nums[i] == 0) {
+                    count = 0;
+
+                }
+                else{
+                    count++;
+                    result =Math.max(result,count);
+                }
+            }
+
+            return result;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //    static void print(int n)
 //    {
@@ -21,27 +64,27 @@ public class Main {
 //        print(n+1);
 //    }
 
-        public static void constructTriangle(int[] A)
-        {
-            //base condition
-            if(A.length < 1)
-
-                return;
-
-            //creating new arr
-            int[] temp = new int[A.length-1];
-            for (int i = 0; i < A.length-1;i++)
-            {
-                int x = A[i]+A[i+1];
-                temp[i] = x;
-            }
-
-            constructTriangle(temp);
-
-            System.out.println(Arrays.toString(A));
-
-
-        }
+//        public static void constructTriangle(int[] A)
+//        {
+//            //base condition
+//            if(A.length < 1)
+//
+//                return;
+//
+//            //creating new arr
+//            int[] temp = new int[A.length-1];
+//            for (int i = 0; i < A.length-1;i++)
+//            {
+//                int x = A[i]+A[i+1];
+//                temp[i] = x;
+//            }
+//
+//            constructTriangle(temp);
+//
+//            System.out.println(Arrays.toString(A));
+//
+//
+//        }
 
 
 }
